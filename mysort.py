3d8b25793da170abf4d20225eaa46e59d1b1b6c1
Jeongmin Oh <battlerhythm@gmail.com>
2018-09-06 01:34:56 -0400

    quicksort update

--- mysort.py
+++ mysort.py
@@ -1,43 +1,43 @@
 class BubbleSort():
-    def sort(self, l):
-        loopCount = len(l)-1
+    def sort(self, alist):
+        loopCount = len(alist)-1
         while loopCount != 0:
-            for i in range(len(l)-1):
-                if l[i] > l[i+1]:
-                    l[i], l[i+1] = l[i+1], l[i]
+            for i in range(len(alist)-1):
+                if alist[i] > alist[i+1]:
+                    alist[i], alist[i+1] = alist[i+1], alist[i]
             loopCount -= 1
-        return l
+        return alist
 
 class InsertionSort():
-    def sort(self, l):
+    def sort(self, alist):
         i = 0
-        while i < len(l)-1:
+        while i < len(alist)-1:
             j = i
-            while j >= 0 and l[j] > l[j+1]:
-                l[j], l[j+1] = l[j+1], l[j]
+            while j >= 0 and alist[j] > alist[j+1]:
+                alist[j], alist[j+1] = alist[j+1], alist[j]
                 j -= 1
             i += 1
-        return l
+        return alist
 
 class SelectionSort():
-    def sort(self, l):
+    def sort(self, alist):
         ptr = 0
         minVal = None 
-        while ptr < len(l):
-            minVal = l[ptr]
-            for i, e in enumerate(l[ptr+1:], ptr+1):
+        while ptr < len(alist):
+            minVal = alist[ptr]
+            for i, e in enumerate(alist[ptr+1:], ptr+1):
                 if e < minVal:
                     minVal = e
-                    l[ptr], l[i] = l[i], l[ptr]
+                    alist[ptr], alist[i] = alist[i], alist[ptr]
             ptr += 1
-        return l
+        return alist
 
 class MergeSort():
-    def sort(self, l):
-        if len(l) <= 1:
-            return l
-        left = l[:len(l)//2]
-        right = l[len(l)//2:]
+    def sort(self, alist):
+        if len(alist) <= 1:
+            return alist
+        left = alist[:len(alist)//2]
+        right = alist[len(alist)//2:]
         left = self.sort(left)
         right = self.sort(right)
         return self.__merge(left, right)
@@ -58,3 +58,28 @@ class MergeSort():
             result.append(right[0])
             del right[0]
         return result 
+
+class QuickSort():
+    def sort(self, alist):
+        self.__sortHelper(alist, 0, len(alist)-1)
+        return alist
+            
+    def __sortHelper(self, alist, start, end):
+        if start < end:
+            pivot = self.__partition(alist, start, end)
+            self.__sortHelper(alist, start, pivot-1)
+            self.__sortHelper(alist, pivot+1, end)
+        return alist
+
+    def __partition(self, alist, start, end):
+        pivot = end
+        wall = start
+        left = start
+        while left < pivot:
+            if alist[left] < alist[pivot]:
+                alist[wall], alist[left] = alist[left], alist[wall]
+                wall += 1
+            left += 1
+        alist[wall], alist[pivot] = alist[pivot], alist[wall]
+        pivot = wall
+        return pivot
--- test_mysort.py
+++ test_mysort.py
@@ -1,32 +1,38 @@
 import unittest
 import cProfile
-from mysort import BubbleSort, InsertionSort, SelectionSort, MergeSort
+from mysort import BubbleSort, InsertionSort, SelectionSort, MergeSort, QuickSort
 
 class TestSortMethods(unittest.TestCase):
     bb = BubbleSort()
     ins = InsertionSort()
     sel = SelectionSort()
     mrg = MergeSort()
+    qck = QuickSort()
 
     def test_BubbleSort(self):
-        l = [e for e in reversed(range(5))]
-        l = self.bb.sort(l)
-        self.assertEqual(l, [e for e in range(5)])
+        alist = [e for e in reversed(range(5))]
+        alist = self.bb.sort(alist)
+        self.assertEqual(alist, [e for e in range(5)])
 
     def test_InsertionSort(self):
-        l = [e for e in reversed(range(5))]
-        l = self.ins.sort(l)
-        self.assertEqual(l, [e for e in range(5)])
+        alist = [e for e in reversed(range(5))]
+        alist = self.ins.sort(alist)
+        self.assertEqual(alist, [e for e in range(5)])
 
     def test_SelectionSort(self):
-        l = [e for e in reversed(range(5))]
-        l = self.sel.sort(l)
-        self.assertEqual(l, [e for e in range(5)])
+        alist = [e for e in reversed(range(5))]
+        alist = self.sel.sort(alist)
+        self.assertEqual(alist, [e for e in range(5)])
 
     def test_MergeSort(self):
-        l = [e for e in reversed(range(5))]
-        l = self.mrg.sort(l)
-        self.assertEqual(l, [e for e in range(5)])
+        alist = [e for e in reversed(range(5))]
+        alist = self.mrg.sort(alist)
+        self.assertEqual(alist, [e for e in range(5)])
+
+    def test_QuickSort(self):
+        alist = [e for e in reversed(range(5))]
+        alist = self.qck.sort(alist)
+        self.assertEqual(alist, [e for e in range(5)])
 
 if __name__ == '__main__':
     unittest.main()
