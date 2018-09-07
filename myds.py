8a39f1b2d8498c92eab364a369395a2c3d6e082d
Jeongmin Oh <battlerhythm@gmail.com>
2018-09-06 23:12:36 -0400

    dequeue update, variable name change alist -> items

--- myds.py
+++ myds.py
@@ -1,35 +1,57 @@
 class Stack():
     def __init__(self):
-        self.alist = []
+        self.items = []
 
     def isEmpty(self):
-        return self.alist == []
+        return self.items == []
 
     def size(self):
-        return len(self.alist)
+        return len(self.items)
 
-    def push(self, e):
-        self.alist.append(e)
+    def push(self, item):
+        self.items.append(item)
 
     def peek(self):
-        return self.alist[-1]
+        return self.items[-1]
 
     def pop(self):
-        return self.alist.pop()
+        return self.items.pop()
 
 class Queue():
     def __init__(self):
-        self.alist = []
+        self.items = []
 
     def isEmpty(self):
-        return self.alist == []
+        return self.items == []
 
     def size(self):
-        return len(self.alist)
+        return len(self.items)
 
-    def enqueue(self, e):
-        self.alist.insert(0, e)
+    def enqueue(self, item):
+        self.items.append(item)
 
     def dequeue(self):
-        return self.alist.pop()
+        return self.items.pop(0)
 
+class Deque():
+    def __init__(self):
+        self.items = []
+
+    def isEmpty(self):
+        return self.items == []
+
+    def size(self):
+        return len(self.items)
+
+    def addFront(self, item):
+        self.items.insert(0, item)
+    
+    def addRear(self, item):
+        self.items.append(item)
+
+    def removeFront(self):
+        return self.items.pop(0)
+
+    def removeRear(self):
+        return self.items.pop()
+        
--- test_myds.py
+++ test_myds.py
@@ -1,9 +1,10 @@
 import unittest
-from myds import Stack, Queue
+from myds import Stack, Queue, Deque
 
 class TestDsMethods(unittest.TestCase):
     stk = Stack()
     q = Queue()
+    dq = Deque()
 
     def test_Stack(self):
         self.stk.push(1)
@@ -25,7 +26,18 @@ class TestDsMethods(unittest.TestCase):
         self.assertEqual(1, self.q.size())
         self.q.dequeue()
         self.assertEqual(True, self.q.isEmpty())
-        
+
+    def test_Deque(self):
+        self.dq.addFront(1)
+        self.dq.addRear('a')
+        self.dq.addFront(2)
+        self.dq.addRear('b')
+        self.assertEqual(4, self.dq.size()) # [2, 1, 'a', 'b']
+        self.assertEqual('b', self.dq.removeRear())
+        self.assertEqual('a', self.dq.removeRear())
+        self.assertEqual(1, self.dq.removeRear())
+        self.assertEqual(2, self.dq.removeFront())
+        self.assertEqual(True, self.dq.isEmpty())
 
 if __name__ == '__main__':
     unittest.main()
