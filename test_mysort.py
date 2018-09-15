370fd0d538d282052800240c82935173687fa1c2
Jeongmin Oh <battlerhythm@gmail.com>
2018-09-14 20:47:53 -0400

    Update: editting test case

--- test_mysort.py
+++ test_mysort.py
@@ -3,28 +3,37 @@ import cProfile
 from mysort import BubbleSort, InsertionSort, SelectionSort, MergeSort, QuickSort
 
 class TestSortMethods(unittest.TestCase):
-    dataSet = [e for e in reversed(range(5))]
     answer = [e for e in range(5)]
 
     def test_BubbleSort(self):
-        alist = self.dataSet
-        self.assertEqual(BubbleSort.sort(alist), self.answer)
+        alist1 = [4, 3, 2, 1, 0]
+        alist2 = [0, 1, 4, 3, 2]
+        self.assertEqual(BubbleSort.sort(alist1), self.answer)
+        self.assertEqual(BubbleSort.sort(alist2), self.answer)
 
     def test_InsertionSort(self):
-        alist = self.dataSet
-        self.assertEqual(InsertionSort.sort(alist), self.answer)
+        alist1 = [4, 3, 2, 1, 0]
+        alist2 = [0, 1, 4, 3, 2]
+        self.assertEqual(InsertionSort.sort(alist1), self.answer)
+        self.assertEqual(InsertionSort.sort(alist2), self.answer)
 
     def test_SelectionSort(self):
-        alist = self.dataSet
-        self.assertEqual(SelectionSort.sort(alist), self.answer)
+        alist1 = [4, 3, 2, 1, 0]
+        alist2 = [0, 1, 4, 3, 2]
+        self.assertEqual(SelectionSort.sort(alist1), self.answer)
+        self.assertEqual(SelectionSort.sort(alist2), self.answer)
 
     def test_MergeSort(self):
-        alist = self.dataSet
-        self.assertEqual(MergeSort.sort(alist), self.answer)
+        alist1 = [4, 3, 2, 1, 0]
+        alist2 = [0, 1, 4, 3, 2]
+        self.assertEqual(MergeSort.sort(alist1), self.answer)
+        self.assertEqual(MergeSort.sort(alist2), self.answer)
 
     def test_QuickSort(self):
-        alist = self.dataSet
-        self.assertEqual(QuickSort.sort(alist), self.answer)
+        alist1 = [4, 3, 2, 1, 0]
+        alist2 = [0, 1, 4, 3, 2]
+        self.assertEqual(QuickSort.sort(alist1), self.answer)
+        self.assertEqual(QuickSort.sort(alist2), self.answer)
 
 if __name__ == '__main__':
     unittest.main()
